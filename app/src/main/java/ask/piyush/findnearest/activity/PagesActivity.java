4df508d37303e4e82e6f4ea70d93c8e20ac21722
akashk1992 <askpi92@gmail.com>
2015-08-23 18:21:25 +0530

    ui refactor.

--- app/src/main/java/ask/piyush/findnearest/activity/PagesActivity.java
+++ app/src/main/java/ask/piyush/findnearest/activity/PagesActivity.java
@@ -1,10 +1,11 @@
 package ask.piyush.findnearest.activity;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -37,9 +38,8 @@ public class PagesActivity extends AppCompatActivity {
     }
 
     private void setUpActionBar() {
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -54,13 +54,39 @@ public class PagesActivity extends AppCompatActivity {
     }
 
     private void renderTermsOfUsesPage() {
-//        actionBar.setTitle("Terms Of Uses");
-        pageContent.setText(Html.fromHtml(getResources().getString(R.string.terms_of_uses_string)));
+        actionBar.setTitle("Terms Of Uses");
+        String htmlAsString = "<b>Terms of uses:</b><br><br>" +
+                "App will require following Permission from User:<br>" +
+                "<i>1) Internet Connection<br>" +
+                "2) User location<br>" +
+                "3) GPS enabled<br></i><br><br>" +
+                "[Note*: We assure you that we ain't sharing your location with anyone and " +
+                "The Find Nearest Team respects user's privacy]";
+        Spanned htmlAsSpanned = Html.fromHtml(htmlAsString);
+        pageContent.setMovementMethod(LinkMovementMethod.getInstance());
+        pageContent.setText(htmlAsSpanned);
+//        pageContent.setText(getResources().getString(R.string.terms_of_uses_string));
     }
 
     private void renderAboutUsPage() {
-//        actionBar.setTitle("About Us");
-        pageContent.setText(Html.fromHtml(getResources().getString(R.string.about_us_string)));
+        actionBar.setTitle("About Us");
+        String htmlAsString = "<b>Find Nearest</b><br><br>" +
+                "developed By Akash Khandale<br>" +
+                "developer id: askplay92<br>" +
+                "contact: askplay92@gmail.com<br>" +
+                "page: androidlearnpoint.blogspot.com<br><br>" +
+                "<i><b>Find Nearest</b> is maps based application," +
+                "that lets you find the nearest place first as per selected category.<br>" +
+                "This app will be very useful for people who travels a lot.<br>" +
+                "Also who are newer in locations." +
+                "So with this app, you can find the nearest place to you with the most precise location" +
+                "and you can explore more about place as well.<br><br>" +
+                "User can also get the exact direction to the place with real time Navigation." +
+                "Not Only one place but user can get direction to related place.";
+        Spanned htmlAsSpanned = Html.fromHtml("" + htmlAsString); // used by TextView
+        pageContent.setMovementMethod(LinkMovementMethod.getInstance());
+        pageContent.setText(htmlAsSpanned);
+//        pageContent.setText(getResources().getString(R.string.about_us_string));
     }
 
     @Override
@@ -71,6 +97,5 @@ public class PagesActivity extends AppCompatActivity {
 
     public void getWidgets() {
         pageContent = (TextView) findViewById(R.id.page_content);
-        pageContent.setTypeface(Typeface.createFromAsset(getAssets(), "font/Roboto-Medium.ttf"));
     }
 }
